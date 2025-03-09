bc430688a99abc1a6c056dcca69a765188defe0e
StivenF2711 <141967258+StivenF2711@users.noreply.github.com>
2025-03-09 18:05:08 -0500

    Update Categoria.jsx

--- src/pages/Categoria.jsx
+++ src/pages/Categoria.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_BACKEND_URL;
+const API_URL = "https://mercadocafetero.up.railway.app";;
 
 function Categorias() {
   const [categorias, setCategorias] = useState([]);

2476a6c6b81d081f3aa926d37d86877eeaa96273
StivenF2711 <141967258+StivenF2711@users.noreply.github.com>
2025-05-23 17:23:44 -0500

    Update InventarioList.jsx
    Co-Authored-By: julianscp <104837697+julianscp@users.noreply.github.com>
    Co-Authored-By: jacobosanchezbrito <176593507+jacobosanchezbrito@users.noreply.github.com>

--- src/components/Inventario/InventarioList.jsx
+++ src/components/Inventario/InventarioList.jsx
@@ -63,6 +63,7 @@ const InventarioList = () => {
 
   useEffect(() => {
     cargarDatos();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const handleAbrirModal = (producto, modo) => {
